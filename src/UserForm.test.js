e25b57e964076d009ca5bfa592b116bb7d5a676a
valentinesamuel <valentinesamuel2580@gmail.com>
2023-03-31 20:56:27 +0100

    INit project

---
+++ src/UserForm.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from "@testing-library/react";
+import User from '@testing-library/user-event'
+import UserForm from "./UserForm";
+
+test('it shows two inputs and a button', () => {
+render(<UserForm/>)
+
+})
