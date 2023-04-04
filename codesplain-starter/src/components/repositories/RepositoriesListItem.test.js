9e54c4bff220a738710a0309b69d818bd93c9ba0
valentinesamuel <valentinesamuel2580@gmail.com>
2023-04-04 18:52:12 +0100

    FEAT: Tested the link to editor page

--- codesplain-starter/src/components/repositories/RepositoriesListItem.test.js
+++ codesplain-starter/src/components/repositories/RepositoriesListItem.test.js
@@ -7,7 +7,7 @@ function renderComponent() {
         full_name: 'facebook/react',
         language: 'JavaScript',
         description: 'A js library',
-        owner: 'facebook',
+        owner: { login: 'facebook' },
         name: 'react',
         html_url: 'https://github.com/facebook/react'
     }
@@ -26,4 +26,19 @@ test('shows a link to the github homepage for this repository', async () => {
         name: /github repository/i
     })
     expect(link).toHaveAttribute('href', repository.html_url)
+})
+
+test('shows a file icon with the appropriate icon', async () => {
+    renderComponent()
+    const icon = await screen.findByRole('img', { name: 'JavaScript' })
+    expect(icon).toHaveClass('js-icon')
+})
+
+test('shows a link to the code editor page', async () => {
+    const { repository } = renderComponent()
+    await screen.findByRole('img', { name: 'JavaScript' })
+    const link = await screen.findByRole('link', {
+        name: new RegExp(repository.owner.login)
+    })
+    expect(link).toHaveAttribute('href', `/repositories/${repository.full_name}`)
 })
