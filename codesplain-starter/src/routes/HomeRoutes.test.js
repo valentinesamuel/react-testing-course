9739e3f0f7f5d0632d6e94ca3c7ae2c29810f129
valentinesamuel <valentinesamuel2580@gmail.com>
2023-04-06 16:46:18 +0100

    FEAT: Cleaned home routes

--- codesplain-starter/src/routes/HomeRoutes.test.js
+++ codesplain-starter/src/routes/HomeRoutes.test.js
@@ -1,6 +1,4 @@
 import { render, screen } from "@testing-library/react";
-import { setupServer } from 'msw/node'
-import { rest } from 'msw';
 import { MemoryRouter } from "react-router";
 import HomeRoute from "./HomeRoute";
 import { createServer } from "../test/server";
