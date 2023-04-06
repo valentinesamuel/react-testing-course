0109764902a0b83ab68a731a5557e21a629b0bbc
valentinesamuel <valentinesamuel2580@gmail.com>
2023-04-06 16:38:20 +0100

    FEAT; Created createserver function

--- codesplain-starter/src/routes/HomeRoutes.test.js
+++ codesplain-starter/src/routes/HomeRoutes.test.js
@@ -3,34 +3,24 @@ import { setupServer } from 'msw/node'
 import { rest } from 'msw';
 import { MemoryRouter } from "react-router";
 import HomeRoute from "./HomeRoute";
+import { createServer } from "../test/server";
 
-
-
-const handlers = [
-    rest.get('/api/repositories', (req, res, ctx) => {
-        const language = req.url.searchParams.get('q').split('language:')[1]
-        return res(
-            ctx.json({
+createServer([
+    {
+        path: '/api/repositories',
+        res: (req) => {
+            const language = req.url.searchParams.get('q').split('language:')[1]
+            return ({
                 items: [
                     { id: 1, full_name: `${language}_one` },
                     { id: 2, full_name: `${language}_two` }
                 ]
             })
-        )
-    })
-]
+        }
+    },
 
-const server = setupServer(...handlers)
+])
 
-beforeAll(() => {
-    server.listen()
-})
-afterEach(() => {
-    server.resetHandlers()
-})
-afterAll(() => {
-    server.close()
-})
 
 test('renders two links for each language', async () => {
     render(
